c6d23f39ae5b4c39cb36459c02079eb78da2e1b2
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-01-29 07:24:28 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -7,23 +7,17 @@ function callMe() {
   return lyric;
 }
 
-function crazy() {
   var thisIsCrazy = function (){
       console.log("hey!!!")
     }
   // fix the code in here:
   thisIsCrazy();
-}
+
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMyName();
-
-  function sayMy() {
-      var name = "Kristin";
-    console.log(name);
-  }
-sayMy();
+  var name = "Kristin";
+  console.log(name);
 }
+
+sayMyName();
